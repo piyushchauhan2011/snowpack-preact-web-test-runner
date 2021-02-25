52b09d3223258e32a41ba78cb37a097ce6e8a219
Piyush Chauhan <piyushchauhan2011@gmail.com>
2021-02-25 23:24:16 +0700

    Add web test runner setup and examples

---
+++ test/calc.test.js
@@ -0,0 +1,7 @@
+import { expect } from "@esm-bundle/chai";
+import { calc } from "../src/calc.js";
+
+it("calculates sums", () => {
+  expect(calc("1 + 1 + 1")).to.equal(3);
+  expect(calc("2 + 6 + 12")).to.equal(20);
+});
