f857cf00cbcda18120257d8a505c9d1a5bbc19ef
Piyush Chauhan <piyushchauhan2011@gmail.com>
2021-02-25 23:39:06 +0700

    Update user to me

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ console.log({
   SNOWPACK_PUBLIC_API_URL: import.meta.env.SNOWPACK_PUBLIC_API_URL,
 });
 
-fetchRepositories("jackfranklin").then((data) => {
+fetchRepositories("piyush").then((data) => {
   render(h(RepoList, { repos: data }, null), document.body);
 });
